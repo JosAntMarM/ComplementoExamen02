a129b1d5faa57ed82f9b6082a7d7169c348ed304
Lon234 <alfred24mesa@gmail.com>
2025-06-26 16:53:08 -0500

    Update Prueba.java

--- Problema02/Prueba.java
+++ Problema02/Prueba.java
@@ -1,1 +1,24 @@
-
+public class Prueba 
+{
+    public static void main(String[] args) 
+    {
+        lista xd = new lista();
+        xd.Insertar(1);
+        xd.Insertar(2);
+        xd.Insertar(4);
+        xd.Imprimir();
+        System.out.println(" ");
+        xd.Guntar();
+        System.out.println(" ");
+        lista xd1 = new lista();
+        xd1.Insertar(2);
+        xd1.Insertar(4);
+        xd1.Insertar(6);
+        xd1.Imprimir();
+        System.out.println(" ");
+        xd1.Guntar();
+        System.out.println(" ");
+        xd.Resta_y_lista(xd, xd1);        
+    }
+    
+}
